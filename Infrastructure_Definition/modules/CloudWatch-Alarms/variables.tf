a4a149ae98c62348313dba749a6f85183d0c9be0
Dhruvin Soni <37222087+DhruvinSoni30@users.noreply.github.com>
2023-09-06 17:55:49 +0530

    Add files via upload

--- Infrastructure_Definition/modules/CloudWatch-Alarms/variables.tf
+++ Infrastructure_Definition/modules/CloudWatch-Alarms/variables.tf
@@ -8,50 +8,62 @@ variable "instancesIds" {
   type = string
 }
 
+# cpuUsageAlarmPeriod
 variable "cpuUsageAlarmPeriod" {
   type = string
 }
 
+# cpuUsageAlarmThreshold
 variable "cpuUsageAlarmThreshold" {
   type = string
 }
 
+# statusFailedAlarmPeriod
 variable "statusFailedAlarmPeriod" {
   type = string
 }
 
+# statusFailedAlarmThreshold
 variable "statusFailedAlarmThreshold" {
   type = string
 }
 
+# memoryUtilizationTimePeriod
 variable "memoryUtilizationTimePeriod" {
   type = string
 }
 
+# memoryUtilizationTimeThreshold
 variable "memoryUtilizationTimeThreshold" {
   type = string
 }
 
+# highCpuUtilizationPeriod
 variable "highCpuUtilizationPeriod" {
   type = string
 }
 
+# highCpuUtilizationThreshold
 variable "highCpuUtilizationThreshold" {
   type = string
 }
 
+# lowCpuUtilizationPeriod
 variable "lowCpuUtilizationPeriod" {
   type = string
 }
 
+# lowCpuUtilizationThreshold
 variable "lowCpuUtilizationThreshold" {
   type = string
 }
 
+# lowCpuCreditBalancePeriod
 variable "lowCpuCreditBalancePeriod" {
   type = string
 }
 
+# lowCpuCreditBalanceThroshold
 variable "lowCpuCreditBalanceThroshold" {
   type = string
 }
